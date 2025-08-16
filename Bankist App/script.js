f869b62f5e6384611bdb00dbd20281eee68a87d1
ShivBuda <shiva20073@hotmail.com>
2025-08-16 06:37:37 +0545

    add Bankist App with index.html and styles.css

---
+++ Bankist App/script.js
@@ -0,0 +1,27 @@
+// Dummy DATA
+const account1 = {
+    owner: "Shiv Bhai",
+    movement: [100, -500, 4000, 100, -45, -34, 60, -100, 800],
+    interestRate: 1.2,
+    pin: 1111,
+};
+const account2 = {
+    owner: "Kale Dai",
+    movement: [100, -50, 4000, 100, -45, -34, 60, -100],
+    interestRate: 1.2,
+    pin: 2222,
+};
+const account3 = {
+    owner: "Fuchche Keta",
+    movement: [100, -50, 4000, 100, -45, -34, 60, -100],
+    interestRate: 1.2,
+    pin: 3333,
+};
+const account4 = {
+    owner: "Tamane Kanchha",
+    movement: [100, -50, 4000, 100, -45, -34, 60, -100],
+    interestRate: 1.2,
+    pin: 4444,
+};
+
+const accounts = [account1, account2, account3, account4];
